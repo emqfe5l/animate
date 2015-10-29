60f4971bb9a317543e1bcf4eb727f14b1fc7c423
ArtemSidun <tomas1772@gmail.com>
2015-10-29 18:19:46 +0200

    AlGol animation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,5 @@
  window.onload = function () {
    animate && animate.train && animate.train.init();
+   animate && animate.letter && animate.letter.init();
    animateUnit && animateUnit.unit && animateUnit.unit.init();
  };
